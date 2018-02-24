919e29c52db23f478307330841f63976e2e98dde
chuntaojun <liaochuntao@live.com>
2018-02-24 23:50:35 +0800

    lstm recommend

---
+++ hyperflex/D-algorithm/svd_recommend.py
@@ -0,0 +1,103 @@
+import numpy as np
+from numpy import linalg as la
+
+
+"""
+svd 矩阵信息压缩，菜谱推荐（传统机器学习算法）
+"""
+
+
+def euclidSim(inA, inB):
+    """
+    欧式距离
+    :param inA:
+    :param inB:
+    :return:
+    """
+    return 1.0 / (1.0 + la.norm(inA - inB))
+
+
+def pearsSim(inA, inB):
+    """
+    皮尔逊相似度
+    :param inA:
+    :param inB:
+    :return:
+    """
+    if len(inA) < 3:
+        return 1.0
+    return 0.5 + 0.5 * np.corrcoef(inA, inB, rowvar=0)[0][1]
+
+
+def cosSim(inA, inB):
+    """
+    余弦相似度
+    :param inA:
+    :param inB:
+    :return:
+    """
+    num = np.float(inA.T * inB)
+    denom = la.norm(inA) * la.norm(inB)
+    return 0.5 + 0.5 * (num / denom)
+
+
+def standEst(dataMat, user, simMeas, item):
+    """
+
+    :param dataMat:
+    :param user:
+    :param simMeas:
+    :param item:
+    :return:
+    """
+    n = np.shape(dataMat)[1]
+    u, sigma, vt = la.svd(dataMat)
+
+    def cul_min_dataMat(sigma):
+        """
+        矩阵压缩信息控制
+        :param sigma:
+        :return:
+        """
+        sigma_2 = sigma ** 2
+        sigma_sum = np.sum(sigma_2) * 0.9
+        for i in range(len(1, sigma_2)):
+            if np.sum(sigma_2[: i]) >= sigma_sum:
+                return np.mat(np.eye(i + 1) * sigma[: i + 1])
+        return dataMat
+    dataMat = cul_min_dataMat(sigma)
+    simTotal = 0.0; ratSimTotal = 0.0
+    for j in range(n):
+        userRating = dataMat[user, j]
+        if userRating == 0:
+            continue
+        overLap = np.nonzero(np.logical_and(dataMat[:, item].A > 0, dataMat[:, j].A > 0))[0]
+        if len(overLap) == 0:
+            similarity = 0
+        else:
+            similarity = simMeas(dataMat[overLap, item], dataMat[overLap, j])
+        simTotal += similarity
+        ratSimTotal += similarity * userRating
+    if simTotal == 0:
+        return 0
+    return ratSimTotal / simTotal
+
+
+def recommend(dataMat, user, N=3, simMeans=cosSim, estMethod=standEst):
+    """
+    推荐结果
+    :param dataMat:
+    :param user:
+    :param N:
+    :param simMeans:
+    :param estMethod:
+    :return:
+    """
+    unrateItems = np.nonzero(dataMat[user, :].A == 0)[1]
+    if len(unrateItems) == 0:
+        return
+    itemScores = []
+    for item in unrateItems:
+        estimatedScore = estMethod(dataMat, user, simMeans, item)
+        itemScores.append(item, estimatedScore)
+    return sorted(itemScores, key=lambda jj: jj[1],reverse=True)[: N]
